830c6d3c0f862717891190dc368881b10392b8a4
Intuiti0n <danielgomescames@gmail.com>
2018-02-05 15:46:00 +0000

    added code to test the h bridge with an Arduino Uno

---
+++ motor_control/test/test/test.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(7, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  delay(1000);
+}
